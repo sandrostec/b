9dfcb61ebe4cd1dec90bb3b250fc0a3f33d64027
Sandro Rodrigues Cordeiro <sandro.rcordeiro@gmail.com>
2024-10-08 17:38:25 -0300

    kkk

---
+++ websocket-clock/server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    let countdown = 1000; // Define o contador regressivo inicial
+
+    // Enviar a contagem regressiva a cada segundo
+    const interval = setInterval(() => {
+        if (countdown >= 0) {
+            ws.send(countdown); // Envia o valor atual do contador
+            countdown--;
+        } else {
+            clearInterval(interval); // Para o contador quando chegar a 0
+        }
+    }, 1000);
+
+    ws.on('close', () => {
+        clearInterval(interval); // Para o intervalo caso o cliente feche a conexão
+    });
+});
+
+console.log("Servidor WebSocket rodando na porta 8080");
